1f8569d6b0fe60770acb20b677a49d93bf755758
鈴木潤一 <junichisuzuki@suzukijunichinoMacBook-puro.local>
2019-06-09 21:59:01 +0900

    [Add]Topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'books/new'
+  get 'top' => 'root#top'
+  post 'books' => 'books#create'
+
+  get 'books' => 'books#index'
 end
